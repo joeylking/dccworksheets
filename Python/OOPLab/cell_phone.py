3a65b56f608bee8e80ad8cb789dc544e477e148b
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-12 10:20:11 -0500

    OOP Lab setup

---
+++ Python/OOPLab/cell_phone.py
@@ -0,0 +1,27 @@
+# As a developer, I want to use Python’s proper snake_case for variable names.
+
+# As a developer, I want to create a CellPhone class.
+
+# As a developer, I want the CellPhone class to have class instance variables to keep track of the CellPhone’s model, phone number, contacts (dictionary), messages (list) and whether the cell phone is on vibrate mode or not.
+
+# As a developer, I want the CellPhone’s model type to be passed into the classes’ initializer via a parameter.
+
+# As a developer, I want the CellPhone class to have a method to receive a text message that prints the message and adds it to the messages list
+
+# As a developer, I want the CellPhone class to have a method to toggle whether the phone is in vibrate mode or not.
+
+# As a developer, I want the CellPhone class to have a method to create and send (print) a new text message to a contact.
+
+# As a developer, I want to import the CellPhone class into main.py so I can instantiate it as a new CellPhone object and interact with it:
+
+# 1. Print the cell phone’s contacts to the terminal
+
+# 2. Send two new messages to the phone through the receive text message method
+
+# 3. Print the cell phone’s messages to the terminal
+
+# 4. Call the create text message method to create a new message
+
+# 5. Toggle the cell phones ringer to vibrate
+
+# 6. Print the cell phone’s current ringer/vibrate setting to the terminal
---
+++ Python/OOPLab/shopping_cart.py
@@ -0,0 +1,37 @@
+# As a developer, I want to use Python’s proper snake_case for variable names.
+
+# As a developer, I want to create a Customer, ShoppingCart, and Product class.
+
+# As a developer, I want the Product class to have class properties to keep track of the Product’s name, price, and category.
+
+# As a developer, I want the Product class’s initializer to take in the initial values for the name, price, and category via parameters
+
+# As a developer, I want the ShoppingCart class to have class properties to keep track of the ShoppingCart’s products (list).
+
+# As a developer, I want the ShoppingCart class to have a method to calculate and return the current total of all products in the cart.
+
+# As a developer, I want the ShoppingCart class to have a method to add a new product to the cart. (Appending to the products list)
+
+# As a developer, I want the ShoppingCart class to have a method to empty all products from the shopping cart.
+
+# As a developer, I want the Customer class to have class instance variables to keep track of the Customer’s name and shopping cart object. (Set the shopping cart instance variable equal to a new ShoppingCart object in the initializer HINT: You will have to import the ShoppingCart class into the customer.py file)
+
+# As a developer, I want the Customer class’s initializer to take in the initial value for the customer’s name via a parameter.
+
+# As a developer, I want the Customer class to have a method to add a new product to the customer’s shopping cart (within this method you will call the shopping cart’s add product method)
+
+# As a developer, I want the Customer class to have a method to view all products in the customer’s shopping cart. (Loop over the shopping cart’s products list and print each product to the terminal)
+
+# As a developer, I want to import the Customer and Product classes into main.py so I can instantiate a customer object as well as three Product objects and interact with the object’s methods:
+
+# 1. Print the customer’s name to the terminal
+
+# 2. Call the customer’s add product to shopping cart method three times and add the three products objects you created
+
+# 3. Call the customer’s view products method
+
+# 4. Call the customer’s shopping cart’s get cart total method. Capture the total the method returns in a variable and print to the terminal
+
+# 5. Call the customer’s shopping cart’s empty cart method
+
+# 6. Check if all products have been removed from the shopping cart
