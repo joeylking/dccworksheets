e6007ca1cdaae405a29b95f3d87f87a81bad7d2a
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-12 14:14:23 -0500

    cleanup

--- Python/OOPLab/product.py
+++ Python/OOPLab/product.py
@@ -1,6 +1,3 @@
-# As a developer, I want the Product class to have class properties to keep track of the Product’s name, price, and category.
-
-# As a developer, I want the Product class’s initializer to take in the initial values for the name, price, and category via parameters
 
 class Product:
     def __init__(self, name, price, category):
--- Python/OOPLab/shopping_cart.py
+++ Python/OOPLab/shopping_cart.py
@@ -1,13 +1,5 @@
 from product import Product
 
-# As a developer, I want the ShoppingCart class to have class properties to keep track of the ShoppingCart’s products (list).
-
-# As a developer, I want the ShoppingCart class to have a method to calculate and return the current total of all products in the cart.
-
-# As a developer, I want the ShoppingCart class to have a method to add a new product to the cart. (Appending to the products list)
-
-# As a developer, I want the ShoppingCart class to have a method to empty all products from the shopping cart.
-
 class ShoppingCart:
     def __init__(self):
         self.products = []
